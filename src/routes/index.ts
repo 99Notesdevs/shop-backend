649a646857e0dc042d0de2609bdd418d4dc8ad07
Manvir Singh <168892034+manvirsingh1716@users.noreply.github.com>
2025-07-14 00:19:06 +0530

    category routes up

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,12 @@
 import { Router } from "express";
-
+import categoryRouter from "./Category";
 const router = Router();
 
 // health check api
 router.get("/healthCheck", async (req, res) => {
     res.status(200).json({
-      message: "Working fine!",
+      message: "vansh is asshole",
     });
 });
-
+router.use("/category", categoryRouter);
 export default router;
