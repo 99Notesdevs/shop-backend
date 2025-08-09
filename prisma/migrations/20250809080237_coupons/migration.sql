2cc8d3cb016773e924291bc1531ceff573713b29
Vansh Maurya <vanshmaurya17@gmail.com>
2025-08-09 13:38:03 +0530

    resolved

--- prisma/migrations/20250809080237_coupons/migration.sql
+++ prisma/migrations/20250809080237_coupons/migration.sql
@@ -1,11 +1,5 @@
-/*
-  Warnings:
-
-  - Added the required column `salePrice` to the `Product` table without a default value. This is not possible if the table is not empty.
-
-*/
 -- AlterTable
-ALTER TABLE "Product" ADD COLUMN     "salePrice" DOUBLE PRECISION NOT NULL;
+ALTER TABLE "Product" ADD COLUMN     "salePrice" DOUBLE PRECISION;
 
 -- CreateTable
 CREATE TABLE "Coupon" (
